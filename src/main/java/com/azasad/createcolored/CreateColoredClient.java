9864ab0c3af384efe19bd380c976fb67c108fd95
AzaSad <AzaSad1597@gmail.com>
2024-08-11 21:27:45 -0500

    initial commit

---
+++ src/main/java/com/azasad/createcolored/CreateColoredClient.java
@@ -0,0 +1,11 @@
+package com.azasad.createcolored;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateColoredClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
