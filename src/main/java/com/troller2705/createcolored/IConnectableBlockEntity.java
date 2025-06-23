0a3cdb308deacb578c7673e99852ab9a8fb20b0c
troller2705 <cole.groves2705@gmail.com>
2025-06-22 22:46:08 -0400

    All the new tank tops (connected gets used for single and connected)

--- src/main/java/com/troller2705/createcolored/IConnectableBlockEntity.java
+++ src/main/java/com/troller2705/createcolored/IConnectableBlockEntity.java
@@ -5,7 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
 
 
-public interface IConnectableBlockEntity extends IMultiBlockEntityContainer.Fluid {
+public interface IConnectableBlockEntity extends IMultiBlockEntityContainer.Fluidk {
     boolean canConnectWith(BlockPos pos, Level level);
 
     Level getWorld();
